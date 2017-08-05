d89e3d49a94d3593ddac6503fa62cf271d3256ce
shangdi1122 <1399070052@qq.com>
2017-08-05 10:08:34 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/SayGoodBye.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class SayGoodBye {
+  @Test
+    public void print() {
+      System.out.println("这就是我！！！不一样的味道");
+  }
+}
---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/SayHelloWorld.java
@@ -0,0 +1,11 @@
+package cn.bdqn.tangcco.tcmp056;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class SayHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("这不是演习，记住这不是演习！！");
+
+    }
+}
